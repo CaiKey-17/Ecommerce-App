7eb6aac4f15a54ed1e38c304b2979b781fed91b9
CaiKey-17 <caoky.sonha@gmail.com>
2025-04-28 17:15:27 +0700

    Websocket demo

--- source/app/lib/services/api_service.dart
+++ source/app/lib/services/api_service.dart
@@ -39,7 +39,7 @@ class ApiResponse<T> {
   }
 }
 
-@RestApi(baseUrl: "http://192.168.70.182:8080/api")
+@RestApi(baseUrl: "http://172.20.10.3:8080/api")
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
